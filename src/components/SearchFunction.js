cc212b9744ee12a4a07bfe7dd421c88c50178b5a
dannychristo <daniel.christopher99@gmail.com>
2025-06-10 21:21:19 +0700

    Static asset and structure setup

---
+++ src/components/SearchFunction.js
@@ -0,0 +1,8 @@
+<form>
+    <input id="searchBar"/>
+    <div id="searchCat">
+        <button id="title"></button>
+        <button id="artist"></button>
+        <button id="album"></button>
+    </div>
+</form>                         
